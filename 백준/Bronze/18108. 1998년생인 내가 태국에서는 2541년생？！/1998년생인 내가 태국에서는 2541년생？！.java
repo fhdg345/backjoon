f94a639f62bcd777622a0b709f6861da7162258e
fhdg345 <48307873+fhdg345@users.noreply.github.com>
2023-02-06 16:11:13 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 212 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int bulgi = sc.nextInt();
+        System.out.println(bulgi-543);
+    }
+
+}
+
+
+
