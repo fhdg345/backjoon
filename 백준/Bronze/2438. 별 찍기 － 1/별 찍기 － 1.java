1431155ece66e68d46149aa7bd82cf44950b699d
fhdg345 <48307873+fhdg345@users.noreply.github.com>
2023-02-13 13:49:52 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 304 ms, Memory: 18076 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String star = "*";
+        int cnt = 1;
+        for (int i = 0; i < n ; i++) {
+            for (int j = 0; j < cnt; j++) {
+                System.out.print(star);
+            }
+            System.out.println();
+            cnt++;
+        }
+    }
+}
