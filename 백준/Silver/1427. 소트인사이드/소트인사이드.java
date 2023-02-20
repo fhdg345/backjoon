5b0c2ff55a71d3173cb11391add139f2bbfdfc94
fhdg345 <48307873+fhdg345@users.noreply.github.com>
2023-02-20 22:00:15 +0900

    [Silver V] Title: 소트인사이드, Time: 212 ms, Memory: 17616 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String n = sc.next();
+        Integer[] arr = new Integer[n.length()];
+        for (int i = 0; i < n.length() ; i++) {
+            arr[i]=n.charAt(i)-'0';
+        }
+        Arrays.sort(arr,Collections.reverseOrder());
+        for (int i = 0; i < n.length(); i++) {
+            System.out.print(arr[i]);
+        }
+    }
+}
