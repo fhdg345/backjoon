7adce70413b15e461325f143da68a114a25692a3
fhdg345 <48307873+fhdg345@users.noreply.github.com>
2023-03-02 16:46:17 +0900

    [Bronze II] Title: 공 바꾸기, Time: 240 ms, Memory: 18552 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int temp = 0;
+        int [] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i]=i+1;
+        }
+        for (int a = 0; a < m; a++) {
+            int i = sc.nextInt();
+            int j = sc.nextInt();
+            temp = arr[i-1];
+            arr[i-1]=arr[j-1];
+            arr[j-1]=temp;
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
